16c41c441800f058f5790b1eb58cc8faec8715aa
therishikapadia <113783744+therishikapadia@users.noreply.github.com>
2024-03-03 10:45:26 +0530

    Add files via upload

--- Django-Budget-App/budget_project/budget_app/views.py
+++ Django-Budget-App/budget_project/budget_app/views.py
@@ -25,32 +25,6 @@ def generate_plot(request):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# View for index page
-def index(request):
-    expense_items = ExpenseInfo.objects.filter(user_expense=request.user).order_by('-date_added')
-    generate_plot(request)
-    context = {'expense_items': expense_items}
-    return render(request, 'budget_app/index.html', context=context)
-
-
-# Define a function to generate the plot
-# def generate_plot(request):
-#     try:
-#         budget_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(budget=Sum('cost', filter=Q(cost__gt=0)))
-#         expense_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(expenses=Sum('cost', filter=Q(cost__lt=0)))
-#         fig, ax = plt.subplots()
-#         ax.bar(['Expenses', 'Budget'], [abs(expense_total['expenses']), budget_total['budget']], color=['red', 'green'])
-#         ax.set_title('Your total expenses vs total budget')
-#         plt.savefig('budget_app/static/budget_app/expense.jpg')
-#     except TypeError:
-#         print('No data.')
-
-# View for index page
-# def index(request):
-#     expense_items = ExpenseInfo.objects.filter(user_expense=request.user).order_by('-date_added')
-#     generate_plot(request)
-#     context = {'expense_items': expense_items}
-#     return render(request, 'budget_app/index.html', context=context)
 
 # View for adding an item
 def add_item(request):
@@ -60,6 +34,18 @@ def add_item(request):
     ExpenseInfo.objects.create(expense_name=name, cost=expense_cost, date_added=expense_date, user_expense=request.user)
     generate_plot(request)
     return HttpResponseRedirect('app')
+
+# View for index page
+def index(request):
+    expense_items = ExpenseInfo.objects.filter(user_expense=request.user).order_by('-date_added')
+    budget_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(budget=Sum('cost', filter=Q(cost__gt=0)))
+    expense_total = ExpenseInfo.objects.filter(user_expense=request.user).aggregate(expenses=Sum('cost', filter=Q(cost__lt=0)))
+    context = {
+        'expense_items': expense_items,
+        'budget': budget_total['budget'] if budget_total['budget'] else 0,
+        'expenses': abs(expense_total['expenses']) if expense_total['expenses'] else 0
+    }
+    return render(request, 'budget_app/index.html', context=context)
 
 # View for user sign up
 def sign_up(request):
